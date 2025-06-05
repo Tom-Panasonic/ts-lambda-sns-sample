93a3d979818cb7bd2524293b6170762dde716a44
Masato Tanaka <tanaka.masato@jp.panasonic.com>
2025-06-05 11:17:03 +0000

    feat: Implement sum function and update handler message
    Added a sum function and updated the handler to include random numbers and their sum in the response message.

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,11 +1,16 @@
+const { sum } = require("./sum");
+
 exports.handler = async () => {
   console.log("Hello World!");
   // 必要に応じて、他の処理を追加できます。
+  const a = Math.floor(Math.random() * 100) + 1;
+  const b = Math.floor(Math.random() * 100) + 1;
+  const result = sum(a, b);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "I am Shiun San!",
+      message: `I am Shiun smk!, ${a} + ${b} = ${result}`,
     }),
   };
 };
