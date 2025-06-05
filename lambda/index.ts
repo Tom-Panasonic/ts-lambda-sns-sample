60c0c6b78004d319c6760bc607449693f8b0e4a0
Masato Tanaka <tanaka.masato@jp.panasonic.com>
2025-06-05 11:27:24 +0000

    feat: Update log messages and add log group for Lambda function
    Updated the log message in the Lambda handler and added a log group configuration in the stack.

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,7 +1,7 @@
 const { sum } = require("./sum");
 
 exports.handler = async () => {
-  console.log("Hello World!");
+  console.log("Hello World! Iam Shiun smk!");
   // 必要に応じて、他の処理を追加できます。
   const a = Math.floor(Math.random() * 100) + 1;
   const b = Math.floor(Math.random() * 100) + 1;
--- lib/ts-lambda-sns-sample-stack.ts
+++ lib/ts-lambda-sns-sample-stack.ts
@@ -2,12 +2,19 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "path";
+import * as logs from "aws-cdk-lib/aws-logs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TsLambdaSnsSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // デフォルトは、lambdaはfunctionNameのlogGroupを作成するが、明示的に作成することも可能
+    const logGroup = new logs.LogGroup(this, "HelloWorldShiunLambdaLogGroup", {
+      logGroupName: "/aws/lambda/CDK-Making-hello-world-shiun-lambda",
+      retention: logs.RetentionDays.THREE_MONTHS, // ログの保持期間を3ヶ月に設定, 本番系は1年
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
     // TypeScriptで記述したLambda関数をデプロイする場合、NodejsFunctionを使うと自動でTypeScriptをesbuildでバンドル
     const lambda = new NodejsFunction(this, "Hello-World-Shiun-Lambda", {
       functionName: `hello-world-shiun-lambda`,
@@ -18,6 +25,7 @@ export class TsLambdaSnsSampleStack extends cdk.Stack {
       architecture: cdk.aws_lambda.Architecture.X86_64,
       timeout: cdk.Duration.seconds(300),
       description: "Hello World Lambda function",
+      logGroup: logGroup, // ここで明示的にロググループを指定
     });
   }
 }
